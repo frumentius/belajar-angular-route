16b456b720a64c859ded59a0e85aff0fe263da58
Frumentius <frumentius_sonic@yahoo.com>
2023-05-28 21:06:44 +0700

    First commit.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  { path: 'first-component', component: FirstComponent },
+  { path: 'second-component', component: SecondComponent },
+  { path: '', component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
