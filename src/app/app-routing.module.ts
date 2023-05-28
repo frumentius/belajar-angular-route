87c48964f18aa6c5b2b40c64f3d4646269c08362
Frumentius <frumentius_sonic@yahoo.com>
2023-05-28 21:39:00 +0700

    Nested route for first component children.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,27 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { FirstComponent } from './first/first.component';
+import { ChildAComponent } from './first/child-a/child-a.component';
+import { ChildBComponent } from './first/child-b/child-b.component';
 import { SecondComponent } from './second/second.component';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
-  { path: 'first-component', component: FirstComponent },
+  {
+    path: 'first-component',
+    component: FirstComponent, // this is the component with the <router-outlet> in the template
+    children: [
+      {
+        path: 'child-a', // child route path
+        component: ChildAComponent, // child route component that the router renders
+      },
+      {
+        path: 'child-b',
+        component: ChildBComponent, // another child route component that the router renders
+      },
+    ],
+  },
   { path: 'second-component', component: SecondComponent },
   { path: '', component: HomeComponent },
   { path: '**', component: NotFoundComponent }
--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
+  standalone: true,
+  imports: [RouterModule],
   selector: 'app-first',
   templateUrl: './first.component.html',
   styleUrls: ['./first.component.css']
