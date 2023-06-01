94293c13927d76237400edfaf3658c54cb7f579c
Frumentius <frumentius_sonic@yahoo.com>
2023-06-01 17:25:12 +0700

    Rename Project.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'belajar-bootstrap-route'`, () => {
+  it(`should have as title 'belajar-angular-route'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('belajar-bootstrap-route');
+    expect(app.title).toEqual('belajar-angular-route');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('belajar-bootstrap-route app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('belajar-angular-route app is running!');
   });
 });
